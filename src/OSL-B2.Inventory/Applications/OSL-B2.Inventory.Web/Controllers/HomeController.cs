49598ebbfee00304efef92cb75dbb1e579e2f23f
Riad <riadofficial.me@gmail.com>
2022-10-22 11:30:29 +0600

    #2 log4net

--- src/OSL-B2.Inventory/Applications/OSL-B2.Inventory.Web/Controllers/HomeController.cs
+++ src/OSL-B2.Inventory/Applications/OSL-B2.Inventory.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,7 @@ namespace WholeSale.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(HomeController));
         public ActionResult Index()
         {
             return View();
