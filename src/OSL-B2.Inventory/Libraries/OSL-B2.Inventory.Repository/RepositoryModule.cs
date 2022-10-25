59d392c9622aefe605cfdc86a301d0c423242c5d
Riad <riadofficial.me@gmail.com>
2022-10-25 11:43:57 +0600

    #12 register modules

---
+++ src/OSL-B2.Inventory/Libraries/OSL-B2.Inventory.Repository/RepositoryModule.cs
@@ -0,0 +1,13 @@
+﻿using OSL_B2.Inventory.Repository.DbContexts;
+using Unity;
+
+namespace OSL_B2.Inventory.Repository
+{
+    public static class RepositoryModule
+    {
+        public static void Register(UnityContainer container)
+        {
+            container.RegisterType<IIMSDbContext, IMSDbContext>();
+        }
+    }
+}
