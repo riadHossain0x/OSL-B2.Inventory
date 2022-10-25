95962a37f3bfdb9e91cf40469b0f8b30184b5100
Riad <riadofficial.me@gmail.com>
2022-10-25 17:30:34 +0600

    #11 factory

---
+++ src/OSL-B2.Inventory/Applications/OSL-B2.Inventory.Web/Factory.cs
@@ -0,0 +1,18 @@
+﻿using OSL_B2.Inventory.Membership;
+using OSL_B2.Inventory.Repository;
+using System.ComponentModel;
+using Unity;
+
+namespace OSL_B2.Inventory.Web
+{
+    public static class Factory<T>
+    {
+        public static T GetInstance()
+        {
+            var container = new UnityContainer();
+            MembershipModule.Register(container);
+            RepositoryModule.Register(container);
+            return container.Resolve<T>();
+        }
+    }
+}
