917e738a8e4cb3b443880f33329beaf598925a45
Riad <riadofficial.me@gmail.com>
2022-11-02 12:43:12 +0600

    service error handle

--- src/OSL-B2.Inventory/Libraries/OSL-B2.Inventory.Service/CategoryService.cs
+++ src/OSL-B2.Inventory/Libraries/OSL-B2.Inventory.Service/CategoryService.cs
@@ -18,7 +18,6 @@ namespace OSL_B2.Inventory.Service
         #endregion
 
         #region Load instances
-        IList<CategoryDto> LoadAllCategories();
         (int total, int totalDisplay, IList<CategoryDto> records) LoadAllCategories(string searchBy, int length, int start, string sortBy, string sortDir);
         #endregion
 
@@ -39,87 +38,120 @@ namespace OSL_B2.Inventory.Service
         #endregion
 
         #region Load instances
-        public IList<CategoryDto> LoadAllCategories()
-        {
-            var entities = _categoryRepository.LoadAll();
-            var entitiesDto = Mapper.Map<IList<CategoryDto>>(entities);
-            return entitiesDto;
-        }
-
         public (int total, int totalDisplay, IList<CategoryDto> records) LoadAllCategories(string searchBy = null, int length = 10, int start = 1, string sortBy = null, string sortDir = null)
         {
-            Expression<Func<Category, bool>> filter = null;
-            if (searchBy != null)
+            try
             {
-                filter = x => x.Name.Contains(searchBy);
-            }
-            var result = _categoryRepository.LoadAll(filter, null, start, length, sortBy, sortDir);
+                Expression<Func<Category, bool>> filter = null;
+                if (searchBy != null)
+                {
+                    filter = x => x.Name.Contains(searchBy);
+                }
+                var result = _categoryRepository.LoadAll(filter, null, start, length, sortBy, sortDir);
 
-            List<CategoryDto> customers = new List<CategoryDto>();
-            foreach (Category course in result.data)
+                List<CategoryDto> customers = new List<CategoryDto>();
+                foreach (Category course in result.data)
+                {
+                    customers.Add(Mapper.Map<CategoryDto>(course));
+                }
+
+                return (result.total, result.totalDisplay, customers);
+            }
+            catch (Exception ex)
             {
-                customers.Add(Mapper.Map<CategoryDto>(course));
-            }
 
-            return (result.total, result.totalDisplay, customers);
+                throw;
+            }
         }
         #endregion
 
         #region Operations
         public void AddCategory(CategoryDto item)
         {
-            var count = _categoryRepository.GetCount(x => x.Name == item.Name && x.IsActive == Status.Active);
+            try
+            {
+                var count = _categoryRepository.GetCount(x => x.Name == item.Name && x.IsActive == Status.Active);
 
-            if (count > 0)
-                throw new InvalidOperationException("There is a category with same name already exist.");
+                if (count > 0)
+                    throw new InvalidOperationException("There is a category with same name already exist.");
 
-            var entity = Mapper.Map<Category>(item);
+                var entity = Mapper.Map<Category>(item);
 
-            _categoryRepository.Add(entity);
-            _categoryRepository.SaveChanages();
+                _categoryRepository.Add(entity);
+                _categoryRepository.SaveChanages();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
         }
 
         public void EditCategory(CategoryDto entity)
         {
-            if (entity == null)
-                throw new InvalidOperationException("There is no category found.");
+            try
+            {
+                if (entity == null)
+                    throw new InvalidOperationException("There is no category found.");
 
-            var count = _categoryRepository.GetCount(x => x.Name == entity.Name && x.IsActive == Status.Active && x.Id != entity.Id);
-            if (count > 0)
-                throw new InvalidOperationException("There is a category with same name already exist.");
+                var count = _categoryRepository.GetCount(x => x.Name == entity.Name && x.IsActive == Status.Active && x.Id != entity.Id);
+                if (count > 0)
+                    throw new InvalidOperationException("There is a category with same name already exist.");
 
-            var category = Mapper.Map<Category>(entity);
-            _categoryRepository.Edit(category);
-            _categoryRepository.SaveChanages();
+                var category = Mapper.Map<Category>(entity);
+                _categoryRepository.Edit(category);
+                _categoryRepository.SaveChanages();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
         }
 
         public void RemoveCategory(long id)
         {
-            var entity = _categoryRepository.GetById(id, "Products");
+            try
+            {
+                var entity = _categoryRepository.GetById(id, "Products");
 
-            if (entity == null)
-                throw new InvalidOperationException("There is no category found.");
+                if (entity == null)
+                    throw new InvalidOperationException("There is no category found.");
 
-            if (entity.Products.Count > 0)
-                throw new InnerElementException($"There are same products under '{entity.Name}' category. Please delete those product and try again.");
+                if (entity.Products.Count > 0)
+                    throw new InnerElementException($"There are same products under '{entity.Name}' category. Please delete those product and try again.");
 
-            entity.IsActive = Status.Inactive;
-            _categoryRepository.Edit(entity);
-            _categoryRepository.SaveChanages();
+                entity.IsActive = Status.Inactive;
+                _categoryRepository.Edit(entity);
+                _categoryRepository.SaveChanages();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
         }
         #endregion
 
         #region Single instances
         public CategoryDto GetCategory(long id)
         {
-            var count = _categoryRepository.GetCount(x => x.Id == id && x.IsActive == Status.Active);
+            try
+            {
+                var count = _categoryRepository.GetCount(x => x.Id == id && x.IsActive == Status.Active);
 
-            if (count == 0)
-                throw new InvalidOperationException("There is no category found.");
+                if (count == 0)
+                    throw new InvalidOperationException("There is no category found.");
 
-            var entity = _categoryRepository.GetById(id);
-            var entityDto = Mapper.Map<CategoryDto>(entity);
-            return entityDto;
+                var entity = _categoryRepository.GetById(id);
+                var entityDto = Mapper.Map<CategoryDto>(entity);
+                return entityDto;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
         } 
         #endregion
 
--- src/OSL-B2.Inventory/Libraries/OSL-B2.Inventory.Service/CustomerService.cs
+++ src/OSL-B2.Inventory/Libraries/OSL-B2.Inventory.Service/CustomerService.cs
@@ -39,76 +39,116 @@ namespace OSL_B2.Inventory.Service
         #region Operations
         public void AddCustomer(CustomerDto customer)
         {
-            var count = _customerRepository.GetCount(x => x.Mobile == customer.Mobile && x.IsActive == Status.Active);
+            try
+            {
+                var count = _customerRepository.GetCount(x => x.Mobile == customer.Mobile && x.IsActive == Status.Active);
 
-            if (count > 0)
-                throw new InvalidOperationException("There is a customer with same mobile number already exist.");
+                if (count > 0)
+                    throw new InvalidOperationException("There is a customer with same mobile number already exist.");
 
-            var entity = Mapper.Map<Customer>(customer);
+                var entity = Mapper.Map<Customer>(customer);
 
-            _customerRepository.Add(entity);
-            _customerRepository.SaveChanages();
+                _customerRepository.Add(entity);
+                _customerRepository.SaveChanages();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
         }
 
         public void EditCustomer(CustomerDto entity)
         {
-            if (entity == null)
-                throw new InvalidOperationException("There is no customer found.");
+            try
+            {
+                if (entity == null)
+                    throw new InvalidOperationException("There is no customer found.");
 
-            var count = _customerRepository.GetCount(x => x.Mobile == entity.Mobile && x.IsActive == Status.Active && x.Id != entity.Id);
-            if (count > 0)
-                throw new InvalidOperationException("There is a customer with same name already exist.");
+                var count = _customerRepository.GetCount(x => x.Mobile == entity.Mobile && x.IsActive == Status.Active && x.Id != entity.Id);
+                if (count > 0)
+                    throw new InvalidOperationException("There is a customer with same name already exist.");
 
-            var customer = Mapper.Map<Customer>(entity);
-            _customerRepository.Edit(customer);
-            _customerRepository.SaveChanages();
+                var customer = Mapper.Map<Customer>(entity);
+                _customerRepository.Edit(customer);
+                _customerRepository.SaveChanages();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
         }
 
         public void RemoveCustomer(long id)
         {
-            var entity = _customerRepository.GetById(id);
+            try
+            {
+                var entity = _customerRepository.GetById(id);
 
-            if (entity == null)
-                throw new InvalidOperationException("There is no customer found.");
+                if (entity == null)
+                    throw new InvalidOperationException("There is no customer found.");
 
-            entity.IsActive = Status.Inactive;
-            _customerRepository.Edit(entity);
-            _customerRepository.SaveChanages();
+                entity.IsActive = Status.Inactive;
+                _customerRepository.Edit(entity);
+                _customerRepository.SaveChanages();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
         }
         #endregion
 
         #region Single instances
         public CustomerDto GetCustomer(long id)
         {
-            var count = _customerRepository.GetCount(x => x.Id == id && x.IsActive == Status.Active);
+            try
+            {
+                var count = _customerRepository.GetCount(x => x.Id == id && x.IsActive == Status.Active);
 
-            if (count == 0)
-                throw new InvalidOperationException("There is no category found.");
+                if (count == 0)
+                    throw new InvalidOperationException("There is no category found.");
 
-            var entity = _customerRepository.GetById(id);
-            var entityDto = Mapper.Map<CustomerDto>(entity);
-            return entityDto;
+                var entity = _customerRepository.GetById(id);
+                var entityDto = Mapper.Map<CustomerDto>(entity);
+                return entityDto;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
         }
         #endregion
 
         #region Load instances
         public (int total, int totalDisplay, IList<CustomerDto> records) LoadAllCustomers(string searchBy = null, int length = 10, int start = 1, string sortBy = null, string sortDir = null)
         {
-            Expression<Func<Customer, bool>> filter = null;
-            if (searchBy != null)
+            try
             {
-                filter = x => x.Name.Contains(searchBy) || x.Email.Contains(searchBy) 
-                || x.Mobile.Contains(searchBy) || x.Address.Contains(searchBy);
-            }
-            var result = _customerRepository.LoadAll(filter, null, start, length, sortBy, sortDir);
+                Expression<Func<Customer, bool>> filter = null;
+                if (searchBy != null)
+                {
+                    filter = x => x.Name.Contains(searchBy) || x.Email.Contains(searchBy)
+                    || x.Mobile.Contains(searchBy) || x.Address.Contains(searchBy);
+                }
+                var result = _customerRepository.LoadAll(filter, null, start, length, sortBy, sortDir);
 
-            List<CustomerDto> customers = new List<CustomerDto>();
-            foreach (Customer course in result.data)
+                List<CustomerDto> customers = new List<CustomerDto>();
+                foreach (Customer course in result.data)
+                {
+                    customers.Add(Mapper.Map<CustomerDto>(course));
+                }
+
+                return (result.total, result.totalDisplay, customers);
+            }
+            catch (Exception ex)
             {
-                customers.Add(Mapper.Map<CustomerDto>(course));
-            }
 
-            return (result.total, result.totalDisplay, customers);
+                throw;
+            }
         } 
         #endregion
     }
