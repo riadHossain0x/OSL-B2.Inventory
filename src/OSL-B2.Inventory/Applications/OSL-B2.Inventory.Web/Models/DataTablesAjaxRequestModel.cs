e7b1f398d4459ad7f809d9c5295e37cd558cddb8
Riad <riadofficial.me@gmail.com>
2022-11-17 17:36:03 +0600

    datatable ajax model

--- src/OSL-B2.Inventory/Applications/OSL-B2.Inventory.Web/Models/DataTablesAjaxRequestModel.cs
+++ src/OSL-B2.Inventory/Applications/OSL-B2.Inventory.Web/Models/DataTablesAjaxRequestModel.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.Extensions.Primitives;
+using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
+using System.Text;
 using System.Web;
 
 namespace OSL_B2.Inventory.Web.Models
@@ -8,11 +11,6 @@ namespace OSL_B2.Inventory.Web.Models
     public class DataTablesAjaxRequestModel
     {
         private readonly HttpRequestBase _request;
-
-        public DataTablesAjaxRequestModel(HttpRequestBase request)
-        {
-            _request = request;
-        }
 
         public int Start
         {
@@ -38,6 +36,19 @@ namespace OSL_B2.Inventory.Web.Models
             }
         }
 
+        public string HeaderText
+        {
+            get
+            {
+                return _request.Headers["id"];
+            }
+        }
+
+        public DataTablesAjaxRequestModel(HttpRequestBase request)
+        {
+            _request = request;
+        }
+
         public string SortColumn
         {
             get
@@ -60,6 +71,81 @@ namespace OSL_B2.Inventory.Web.Models
             {
                 return _request["order[0][dir]"];
             }
+        }
+
+        public int PageIndex
+        {
+            get
+            {
+                if (Length > 0)
+                    return (Start / Length) + 1;
+                else
+                    return 1;
+            }
+        }
+
+        public int PageSize
+        {
+            get
+            {
+                if (Length == 0)
+                    return 1;
+                else
+                    return Length;
+            }
+        }
+
+        public static object EmptyResult
+        {
+            get
+            {
+                return new
+                {
+                    recordsTotal = 0,
+                    recordsFiltered = 0,
+                    data = (new string[] { }).ToArray()
+                };
+            }
+        }
+
+        public string GetSortText(string[] columnNames)
+        {
+            var method = _request.HttpMethod.ToLower();
+
+            var queryString = _request.QueryString.AllKeys
+                .Select(x => new KeyValuePair<string, StringValues>(x.ToString(), _request.QueryString[x.ToString()]));
+            var queryForm = _request.Form.AllKeys
+                .Select(x => new KeyValuePair<string, StringValues>(x.ToString(), _request.QueryString[x.ToString()]));
+
+            if (method == "get")
+                return ReadValues(queryString, columnNames);
+            else if (method == "post")
+                return ReadValues(queryForm, columnNames);
+            else
+                throw new InvalidOperationException("Http method not supported, use get or post");
+        }
+
+        private string ReadValues(IEnumerable<KeyValuePair<string, StringValues>>
+            requestValues, string[] columnNames)
+        {
+            var sortText = new StringBuilder();
+            for (var i = 0; i < columnNames.Length; i++)
+            {
+                if (requestValues.Any(x => x.Key == $"order[{i}][column]"))
+                {
+                    if (sortText.Length > 0)
+                        sortText.Append(",");
+
+                    var columnValue = requestValues.Where(x => x.Key == $"order[{i}][column]").FirstOrDefault();
+                    var directionValue = requestValues.Where(x => x.Key == $"order[{i}][dir]").FirstOrDefault();
+
+                    var column = int.Parse(columnValue.Value.ToArray()[0]);
+                    var direction = directionValue.Value.ToArray()[0];
+                    var sortDirection = $"{columnNames[column]} {(direction == "asc" ? "asc" : "desc")}";
+                    sortText.Append(sortDirection);
+                }
+            }
+            return sortText.ToString();
         }
     }
 }
